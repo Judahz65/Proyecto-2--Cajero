c68284b3e87454406395e3f3bd06ae11458f045c
Judahz65 <110071797+Judahz65@users.noreply.github.com>
2022-10-29 13:34:28 -0500

    Almost
    Added new users and visual styles
    
    ??Sensei
    -Change the eye
    -Movement of the icons
    -Select whole bar not just the icon

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
 // alert('Mom Im programming');
-// console.log('Hola desde la consola');
 
 // ZONA DE IDENTIFICACION DEL USUARIO
 const verificar=document.getElementById('verificar');
 function ingresar(form){
-    if (form.inputUser.value == 'Juan') {
-        if (form.inputPassword.value== '1234') {
-            console.log('Estas en el usuario');
-            location='main.html';
-        } else{
-            alert('Contraseña incorrecta')
+    if (form.inputUser.value == 'Gera' && form.inputPassword.value== '1234') {
+            console.log('Estas en Gera');  
+            location='main.html'          
+        } else if(form.inputUser.value == 'Mali' && form.inputPassword.value== '0000'){
+            location='mali.html';
         }
-    } else {
-        alert('Usuario no encontrado');
-    }
+     else if(form.inputUser.value == 'Mali') {
+        if (form.inputPassword.value== '0000') {
+            console.log('Estas en el usuario');
+            location='mali.html';
+        }
+     }
+        else if(form.inputUser.value == 'Maui') {
+            if (form.inputPassword.value== '8521') {
+                console.log('Estas en el usuario');
+                location='maui.html';
+            }
+                else{
+                    alert('Usuario y/o contraseña no encontrados');
+                }
+        }
 }
-
-
 
 // ZONA DE ESTADO DE CUENTA
 var enviar= document.getElementById('enviar');
@@ -24,9 +32,7 @@ var depositar = document.getElementById('depositar');
 var verNo = document.getElementById('verNo');
 
 
-
 enviar.addEventListener('click', ()=>{
-    console.log('Le diste click a enviar mi fafa');
     var saldoT = parseInt(document.getElementById('saldoT').value);
     var cantidad = parseInt(document.getElementById('cantidadR').value); 
     if(saldoT>10 && saldoT<990){
@@ -36,11 +42,7 @@ enviar.addEventListener('click', ()=>{
     }    
 })
 
-
-
-
 depositar.addEventListener('click', ()=>{
-    console.log('Le diste click a depositar mi fafa');
     var saldoT = parseInt(document.getElementById('saldoT').value);
     var cantidad = parseInt(document.getElementById('cantidadR').value); 
     if(saldoT>10 && saldoT<990){
@@ -51,6 +53,19 @@ depositar.addEventListener('click', ()=>{
     
 })
 
+verNo.addEventListener('click', ()=>{
+    var visible=0;
+    var monto= document.getElementById('monto').style.display;
+    if(visible=0){
+        monto= 'none';
+        verNo.innerHTML='<a href="#"><i class="fa-solid fa-eye-slash fa-2x"</i></a>  ';
+    }else if(visible=1){
+
+    }
+})
+
+
+// FUNCIONES CAMBIO DE TEXTO
 depositar.addEventListener('mouseover', ()=>{
     document.getElementById("funcion").style.display = "block";
     document.getElementById("funcion").innerHTML = "Depositar";
